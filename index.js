f700ef448bd21859a4a2f12abb036ed370130b08
justbegabs <93821488+justbegabs@users.noreply.github.com>
2023-12-25 21:34:10 -0300

    checkbox

---
+++ index.js
@@ -0,0 +1,51 @@
+// Função para verificar e definir o estado da checkbox
+function verificarEstadoCheckbox(idEl) {
+    // Obtemos o estado salvo no localStorage
+    const estadoSalvo = localStorage.getItem(idEl);
+
+    // Se houver um estado salvo, aplicamos à checkbox
+    if (estadoSalvo) {
+        document.getElementById(idEl).checked = estadoSalvo === 'true';
+    }
+}
+
+// Função chamada quando a checkbox é alterada
+function checkboxAlterada(evt) {
+    console.log(evt.target.id)
+    // Obtemos o estado atual da checkbox
+    const estadoAtual = document.getElementById(evt.target.id).checked;
+
+    // Salvamos o estado no localStorage
+    localStorage.setItem(evt.target.id, estadoAtual);
+}
+
+// Adicionamos um ouvinte de evento para a carga da página
+document.addEventListener('DOMContentLoaded', function () {
+    // Verificamos e definimos o estado da checkbox ao carregar a página
+    verificarEstadoCheckbox('item1');
+    verificarEstadoCheckbox('item2');
+    verificarEstadoCheckbox('item3');
+    verificarEstadoCheckbox('item4');
+    verificarEstadoCheckbox('item5');
+    verificarEstadoCheckbox('item6');
+    verificarEstadoCheckbox('item7');
+    verificarEstadoCheckbox('item8');
+    verificarEstadoCheckbox('item9');
+    verificarEstadoCheckbox('item10');
+    verificarEstadoCheckbox('item11');
+    verificarEstadoCheckbox('item12');
+
+    // Adicionamos um ouvinte de evento para quando a checkbox é alterada
+    document.getElementById('item1').addEventListener('change', checkboxAlterada);
+    document.getElementById('item2').addEventListener('change', checkboxAlterada);
+    document.getElementById('item3').addEventListener('change', checkboxAlterada);
+    document.getElementById('item4').addEventListener('change', checkboxAlterada);
+    document.getElementById('item5').addEventListener('change', checkboxAlterada);
+    document.getElementById('item6').addEventListener('change', checkboxAlterada);
+    document.getElementById('item7').addEventListener('change', checkboxAlterada);
+    document.getElementById('item8').addEventListener('change', checkboxAlterada);
+    document.getElementById('item9').addEventListener('change', checkboxAlterada);
+    document.getElementById('item10').addEventListener('change', checkboxAlterada);
+    document.getElementById('item11').addEventListener('change', checkboxAlterada);
+    document.getElementById('item12').addEventListener('change', checkboxAlterada);
+});
