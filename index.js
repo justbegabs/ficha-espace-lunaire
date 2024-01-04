859ab55ff981c42d210ffa7ba437968acf917866
justbegabs <93821488+justbegabs@users.noreply.github.com>
2024-01-03 21:15:02 -0300

    final.

--- index.js
+++ index.js
@@ -45,6 +45,28 @@ document.addEventListener('DOMContentLoaded', function () {
     verificarEstadoCheckbox('item10');
     verificarEstadoCheckbox('item11');
     verificarEstadoCheckbox('item12');
+    verificarEstadoCheckbox('item13');
+    verificarEstadoCheckbox('item14');
+    verificarEstadoCheckbox('item15');
+    verificarEstadoCheckbox('item16');
+    verificarEstadoCheckbox('item17');
+    verificarEstadoCheckbox('item18');
+    verificarEstadoCheckbox('item19');
+    verificarEstadoCheckbox('item20');
+    verificarEstadoCheckbox('item21');
+    verificarEstadoCheckbox('item22');
+    verificarEstadoCheckbox('item23');
+    verificarEstadoCheckbox('item24');
+    verificarEstadoCheckbox('item25');
+    verificarEstadoCheckbox('item26');
+    verificarEstadoCheckbox('item27');
+    verificarEstadoCheckbox('item28');
+    verificarEstadoCheckbox('item29');
+    verificarEstadoCheckbox('item30');
+    verificarEstadoCheckbox('item31');
+    verificarEstadoCheckbox('item32');
+    verificarEstadoCheckbox('item33');
+
 
     // Adicionamos um ouvinte de evento para quando a checkbox é alterada
     document.getElementById('item1').addEventListener('change', checkboxAlterada);
@@ -59,6 +81,28 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('item10').addEventListener('change', checkboxAlterada);
     document.getElementById('item11').addEventListener('change', checkboxAlterada);
     document.getElementById('item12').addEventListener('change', checkboxAlterada);
+    document.getElementById('item13').addEventListener('change', checkboxAlterada);
+    document.getElementById('item14').addEventListener('change', checkboxAlterada);
+    document.getElementById('item15').addEventListener('change', checkboxAlterada);
+    document.getElementById('item16').addEventListener('change', checkboxAlterada);
+    document.getElementById('item17').addEventListener('change', checkboxAlterada);
+    document.getElementById('item18').addEventListener('change', checkboxAlterada);
+    document.getElementById('item19').addEventListener('change', checkboxAlterada);
+    document.getElementById('item20').addEventListener('change', checkboxAlterada);
+    document.getElementById('item21').addEventListener('change', checkboxAlterada);
+    document.getElementById('item22').addEventListener('change', checkboxAlterada);
+    document.getElementById('item23').addEventListener('change', checkboxAlterada);
+    document.getElementById('item24').addEventListener('change', checkboxAlterada);
+    document.getElementById('item25').addEventListener('change', checkboxAlterada);
+    document.getElementById('item26').addEventListener('change', checkboxAlterada);
+    document.getElementById('item27').addEventListener('change', checkboxAlterada);
+    document.getElementById('item28').addEventListener('change', checkboxAlterada);
+    document.getElementById('item29').addEventListener('change', checkboxAlterada);
+    document.getElementById('item30').addEventListener('change', checkboxAlterada);
+    document.getElementById('item31').addEventListener('change', checkboxAlterada);
+    document.getElementById('item32').addEventListener('change', checkboxAlterada);
+    document.getElementById('item33').addEventListener('change', checkboxAlterada);
+
 });
 
 function alternarVisibilidade(elementId) {
@@ -71,3 +115,23 @@ function alternarVisibilidade(elementId) {
       elementoTexto.style.display = 'none'; // Torna o texto invisível
     }
   }
+
+  function salvarNoLocalStorage() {
+    const texto = document.getElementById('backstory').value;
+
+    // Salva o texto diretamente no localStorage
+    localStorage.setItem('dados_texto', texto);
+
+    alert('Texto salvo no localStorage!');
+}
+
+// Carrega os dados do localStorage ao carregar a página (opcional)
+window.onload = function() {
+    // Recupera o texto diretamente do localStorage
+    const textoSalvo = localStorage.getItem('dados_texto');
+
+    // Verifica se há texto salvo
+    if (textoSalvo) {
+        document.getElementById('backstory').value = textoSalvo;
+    }
+};
